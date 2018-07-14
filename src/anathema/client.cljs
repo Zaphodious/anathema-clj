7abaf28c6b2a0ae494bb3bf2ef2e04085eb15b5a
Alex Chythlook <achythlook@gmail.com>
2018-07-13 17:58:26 -0800

    Add rum to client code

--- src/anathema/client.cljs
+++ src/anathema/client.cljs
@@ -1,3 +1,16 @@
-(ns anathema.client)
+(ns anathema.client
+    (:require [rum.core :as rum]))
+
+(enable-console-print!)
+
+(def *state (atom {}))
+
+(println "do me! do me! Do me again!")
+
+(rum/defc dudebro
+    []
+  [:div "thing"])
 
 (js/console.log "FIXME")
+
+(rum/mount (dudebro) (.querySelector js/document "#app"))
